6fd585c03756da791e7dd251ee9077c43ed66d47
RMB <100899..iw>
2017-01-27 16:00:52 +0100

    Añadido paquete "jshint" y probándolo con app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -36,7 +38,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
 
   res.status(err.status || 500);
 
